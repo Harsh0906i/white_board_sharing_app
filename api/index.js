092673745cd1dcd45fe8904e8bfe67c138ebc55e
Harshit <harshitsingharya24@gmail.com>
2024-07-02 17:26:42 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,26 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const mongoose = require('mongoose')
-const userRouter = require('./routes/user')
-const authRouter = require('./routes/auth')
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173/',
+        origin: 'http://localhost:5173', // Removed trailing slash
         methods: ['GET', 'POST'],
-        allowedHeaders: ['Authorization','Content-Type'],
+        allowedHeaders: ['Authorization', 'Content-Type'],
         credentials: true
     }
 });
@@ -31,6 +31,7 @@ main()
     }).catch((err) => {
         console.log(err);
     });
+
 async function main() {
     await mongoose.connect(process.env.MONGOURI);
 };
@@ -43,9 +44,10 @@ io.on('connection', (socket) => {
         socket.broadcast.to(roomid).emit('whiteboardDataResponse', data);
     });
 });
+
 app.get('/', (req, res) => {
     res.send('working!');
-})
+});
 
 server.listen(8000, () => {
     console.log('Server is working on port 8000');
